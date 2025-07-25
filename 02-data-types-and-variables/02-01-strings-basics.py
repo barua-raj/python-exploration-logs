5acc883331a9fd598068b2e23c7e5052cf149e3c
barua-raj <barua.jovraj@gmail.com>
2025-07-26 04:25:36 +0600

    data type - strings

---
+++ 02-data-types-and-variables/02-01-strings-basics.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 26 04:07:24 2025
+
+@author: Joyraz Barua
+"""
+
+name = str("Mr. James Bond.")
+city = str("He lives in the United Kingdom.")
+print(name)
+print(city)
