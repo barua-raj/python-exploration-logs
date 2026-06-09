543a4d5b5a2505498fad142cb826c5500b40786e
barua-raj <barua.jovraj@gmail.com>
2026-06-09 14:53:40 +0600

    initial push

---
+++ 02-data-types-and-variables/02-01-strings-basics.py
@@ -0,0 +1,4 @@
+name = str("Mr. James Bond.")
+city = str("He lives in the United Kingdom.")
+print(name)
+print(city)
---
+++ hello-world.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("This is not my first python program!")
