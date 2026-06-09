eeadeffd05158a512f2bfa5f3bb1f6c1dc339450
barua-raj <barua.jovraj@gmail.com>
2026-06-09 15:43:56 +0600

    test case

--- 02-data-types-and-variables/02-01-strings-basics.py
+++ 02-data-types-and-variables/02-01-strings-basics.py
@@ -10,5 +10,7 @@ Created on Sat Jul 26 04:07:24 2025
 >>>>>>> 5acc883331a9fd598068b2e23c7e5052cf149e3c
 name = str("Mr. James Bond.")
 city = str("He lives in the United Kingdom.")
+age = str ("30")
 print(name)
 print(city)
+print(age)
